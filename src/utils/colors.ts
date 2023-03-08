bb9924d767fe78c3e5c887cb6a7339abd1f9d33e
lmariano1308 <lucasmariano2000@hotmail.com>
2023-03-08 16:53:37 -0300

    first commit

---
+++ src/utils/colors.ts
@@ -0,0 +1,12 @@
+export const Colors = {
+  primary: '#646ff0',
+  black1: '#646681',
+  black2: '#585858',
+  background1: '#f8f8ff',
+  background2: '#ecedf6',
+  background3: '#cccdde',
+  gray1: '#eee',
+  gray2: '#dedfe1',
+  black: '#000',
+  white: '#fff',
+};
